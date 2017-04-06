dd0a0758735bebea401a646847e84fc1f6817d04
Namuun Bayaraa & Ransom Byers <pair+namuunaa.rasnom@devbootcamp.com>
2017-04-06 15:18:13 -0700

    Add bcrypt to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
 class User < ActiveRecord::Base
+  include BCrypt
+
   has_many :questions
   has_many :answers
+
+  validates :display_name, :email, :password_hash, presence: true
+  validates :display_name, :email, uniqueness: true
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
