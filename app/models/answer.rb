182aa640753ef2923f6c9aefef32e45fbab31724
Govind Rai <email": null>
2017-04-07 19:44:38 -0700

    Add ajax to votes button,plus shit tons of style

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,8 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   has_many :votes, as: :voteable
   has_many :comments, as: :commentable
+
+  def get_total_votes
+    self.votes.sum(:vote_value)
+  end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,30 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  function decreaseAnswerVote(event) {
+    event.preventDefault();
+
+    var insertLocation = $(this).parent().prev();
+    var data = $(this).parent().serialize();
+    var answerID = $(this).prev('input').val();
+
+    var request = $.ajax({
+      url: `/answers/${answerID}/votes`,
+      method: "POST"
+    });
+
+    request.done(function(response) {
+      insertLocation.html(response)
+    })
+
+    request.fail(function(response) {
+      console.log('YOOOOHOOHOHOOHO we screwed up');
+    })
+
+  }
+
+
+  $('.answers-container').on('click', '.answer-down', decreaseAnswerVote)
+
+
+
 });
